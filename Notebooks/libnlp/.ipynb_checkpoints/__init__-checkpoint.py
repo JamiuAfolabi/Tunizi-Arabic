31d8fa7338b309937b648131a01c2e262760bc6a
JamiuAfolabi <68618845+JamiuAfolabi@users.noreply.github.com>
2022-04-24 15:35:29 +0100

    Delete Notebooks/libnlp/.ipynb_checkpoints directory

--- Notebooks/libnlp/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from libnlp import (preprocessing,
-                    transcription,
-                    env
-                   )
-
-__all__=['preprocessing',
-         'Transcribe',
-         'Env'
-        ]
